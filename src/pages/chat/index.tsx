fd1fafa1d1b614fe48150f83792bc8198231c13d
田之脇絃太 <gens9@tanowakigenfutoshinoMacBook-Air.local>
2023-03-19 12:57:16 +0900

    チャット取得機能

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,14 +1,33 @@
 import {
+  Avatar,
+  Box,
   Button,
   chakra,
   Container,
+  Flex,
   Heading,
   Input,
   Spacer,
+  Text,
 } from '@chakra-ui/react'
-import { FormEvent, useState } from 'react'
-import { getDatabase, push, ref} from '@firebase/database'
+import { FormEvent, useEffect, useState } from 'react'
+import { getDatabase, onChildAdded,  push, ref} from '@firebase/database'
 import { FirebaseError } from 'firebase/app'
+
+type MessageProps = {
+  message: string
+}
+
+const Message = ({ message }: MessageProps) => {
+  return (
+    <Flex alignItems={'start'}>
+      <Avatar />
+      <Box ml={2}>
+        <Text bgColor={'gray.200'} rounded={'md'} px={2} py={1}>{message}</Text>
+      </Box>
+    </Flex>
+  )
+}
 
 export const Page = () => {
   const [ message, setMessage ] = useState<string>('')
@@ -26,16 +45,39 @@ export const Page = () => {
       }
     }
   }
+
+  const [ chats, setChats ] = useState<{ message: string }[]>([])
+
+  useEffect(() => {
+    try {
+      const db = getDatabase()
+      const dbRef = ref(db, 'chat')
+      return onChildAdded(dbRef, (snapshot) => {
+        const message = String(snapshot.val()['message'] ?? '')
+        setChats((prev) => [...prev, { message }])
+      })
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        console.error(e)
+      }
+      return
+    }
+  }, [])
+
   return (
-    <Container py={16}>
-      <Heading>
-        チャット
-      </Heading>
-      <Spacer height={8} aria-hidden />
+    <Container py={12}>
+      <Heading>チャット</Heading>
+      <Spacer height={12} aria-hidden />
       <chakra.form display={'flex'} gap={2} onSubmit={handleSendMessage}>
         <Input value={message} onChange={(e) => setMessage(e.target.value)} />
         <Button type={'submit'}>送信</Button>
       </chakra.form>
+      <Spacer height={16} aria-hidden />
+      <Flex flexDirection={'column'} overflowY={'auto'} gap={2} height={400}>
+        {chats.map((chat, index) => (
+          <Message message={chat.message} key={`ChatMessage_${index}`} />
+        ))}
+      </Flex>
     </Container>
   )
 }
